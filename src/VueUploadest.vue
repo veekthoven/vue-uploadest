08e0f1bbd589f9f4b8a8ff21d69994f7938a3a66
Victor Abbah Nkoms <veekthoven@gmail.com>
2020-03-21 17:43:33 +0100

    arrow functions

--- src/VueUploadest.vue
+++ src/VueUploadest.vue
@@ -29,7 +29,7 @@
 			},
 			accept: { // accepted file mime type or extension. array if multiple, string if single
 				type: String,
-				default: null
+				default: null //Accept every type of file
 			},
 			formData: { // Form data to be sent along with the file
 				type: Object,
@@ -37,57 +37,127 @@
 				default: () => {
 					return {}
 				}
+			},
+			headers: {
+				type: Object,
+				required: false,
+				default: () => {
+					return {}
+				}
+			},
+			uploadMultiple: {
+				type: Boolean,
+				default: false
+			},
+			maxFileSize: {
+				type: Number,
+				default: 256 //MB
+			},
+			autoStart: {
+				type: Boolean,
+				default: true
 			}
 		},
 		data () {
 			return {
-				uploading: false,
-				progress: 0,
-				error: '',
+				dropzone: null,
+				totalProgress: 0,
 				imgSrc: '',
-				successful: false
 			}
 		},
 		render () {
 			return this.$scopedSlots.default({
-				uploading: this.uploading,
-				progress: this.progress,
+				files: this.dropzone ? this.dropzone.files : [],
+				totalProgress: this.totalProgress,
 				error: this.error,
 				imgSrc: this.imgSrc,
-				successful: this.successful
+				removeFile: this.removeFile,
+				start: this.start,
+				startAll: this.startAll,
+				cancel: this.cancel
 			});
 		},
 		mounted () {
-			new Dropzone(`#${this.id}` , { 
+			this.dropzone = new Dropzone(`#${this.id}` , { 
 				url: this.action,
 				paramName: this.name,
-				clickable: `#${this.clickable}`,
+				clickable: document.querySelector(`#${this.clickable}`) ? `#${this.clickable}` : true,
 				acceptedFiles: this.accept,
+				uploadMultiple: this.uploadMultiple,
+				maxFilesize: this.maxFileSize,
+				autoQueue: this.autoStart,
 				previewsContainer: false,
 				headers: {
-					'Accept': 'application/json'
+					'Accept': 'application/json',
+					...this.headers
 				},
 				params: this.formData
 			})
-			.on('addedfile', () => {
+			.on('addedfile', (file) => {
 				this.uploading = true;
-			})
-			.on('uploadprogress', (file, progress) => {
-				this.progress = progress;
+				this.$emit('fileAdded', file);
 			})
 			.on("success", (file, response) => {
-				this.$emit('success', response);
-				this.successful = true;
-				this.uploading = false;
+				this.$emit('success', file, response);
 			})
 			.on("thumbnail", (file, dataURL) => {
 				this.imgSrc = dataURL;
 			})
-			.on('error', (file, response) => {
-				this.$emit('error', response)
-				this.successful = false
-				this.uploading = false
-			});
+			.on('error', (file, errorMessage, xhr) => {
+				this.$emit('error', file, errorMessage, xhr)
+			})
+			.on('removedfile', (file) => {
+				this.$emit('removedFile', file)
+			})
+			.on('complete', (file) => {
+				this.$emit('completed', file)
+			})
+			.on('canceled', (file) => {
+				this.$emit('canceled', file)
+			})
+			.on('successmultiple', (files) => {
+				this.$emit('successAll', files)
+			})
+			.on('completemultiple', (files) => {
+				this.$emit('completedAll', files)
+			})
+			.on('cancelmultiple', (files) => {
+				this.$emit('canceledAll', files)
+			})
+			.on('totaluploadprogress', (progress, totalBytes, totalBytesSent) => {
+				this.totalProgress = progress;
+				this.$emit('progressAll', progress, totalBytes, totalBytesSent)
+			})
+		},
+		methods: {
+			removeFile(file) {
+				try{
+					this.dropzone.removeFile(file);
+				}catch(e) {
+					console.log(e.toString())
+				}
+			},
+			cancel() {
+				try{
+					this.dropzone.removeAllFiles(true);
+				}catch(e) {
+					console.log(e.toString())
+				}
+			},
+			startAll() {
+				if (!this.dropzone.files.length) return;
+
+				this.dropzone.files.forEach(file => {
+					this.start(file);	
+				});
+			},
+			start(file) {
+				try{
+					this.dropzone.enqueueFile(file);
+				}catch(e) {
+					console.log(e.toString())
+				}
+			}
 		}
 	}
 </script>
